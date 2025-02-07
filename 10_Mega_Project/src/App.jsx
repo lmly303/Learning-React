c2c636d32385fa711e32559744033f135c76efe1
Shrey Kariria <shrey.kariria1910@gmail.com>
2025-02-07 23:44:54 +0530

    component creation 1

--- 10_Mega_Project/src/App.jsx
+++ 10_Mega_Project/src/App.jsx
@@ -1,15 +1,49 @@
-
+import {useDispatch} from 'react-redux'
+import { useState } from 'react'
 import './App.css'
+import authService from './appwrite/auth'
+import { useEffect } from 'react'
+import { login, logout } from './store/authSlice'
+import Header from './components/Header/Header'
+import Footer from './components/Footer/Footer'
+import {Outlet} from 'react-router-dom'
 
 function App() {
   
-  console.log(import.meta.env.VITE_APPWRITE_URL)
+  const [loading,setLoading] = useState(true)
+  const dispatch = useDispatch();
 
-  return (
-    <>
-      <h1>BLOG app with appwrite</h1>
-    </>
-  )
+  useEffect(()=>{
+    authService.getCurrentUser()
+      .then((userData)=>{
+        if(userData){
+          dispatch(login({userData}))
+        }
+        else{
+          dispatch(logout())
+        }
+      })
+      .catch((error) => {
+        console.error("Failed to fetch user data:", error);
+        dispatch(logout())
+      })  
+      .finally(()=> setLoading(false))
+  },[dispatch]);
+
+  
+
+  return !loading ? 
+    (
+      <div className='min-h-screen flex flex-wrap content-between bg-gray-400'>
+        <div className='w-full block'>
+          <Header />
+          <main>
+            TODO: <Outlet />
+          </main>
+          <Footer />
+        </div>
+      </div>
+    ) : null
 }
 
 export default App
